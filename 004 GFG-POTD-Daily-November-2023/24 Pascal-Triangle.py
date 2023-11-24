343883f65b7152c22682395da0fd644f8b7d5495
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2023-11-24 19:14:35 +0530

    November-2023

---
+++ 004 GFG-POTD-Daily-November-2023/24 Pascal-Triangle.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+
+    def nthRowOfPascalTriangle(self,n):
+        # code here
+        mod = ((10**9) + 7)
+        results = [[] for i in range (n+1)]
+        #resuls[1] = [1]
+        if n == 1 : return [1]
+        results[2] = [1,1]
+        for i in range(3, n+1):
+            results[i].append(1)
+            prev = results[i-1]
+            leftOne = 0
+            rightOne = 1
+            while(rightOne < (i - 1)):
+                results [i] .append((prev[leftOne] + prev[rightOne]) % mod)
+                leftOne += 1
+                rightOne += 1
+            results[i].append(1)
+        return results[n]
