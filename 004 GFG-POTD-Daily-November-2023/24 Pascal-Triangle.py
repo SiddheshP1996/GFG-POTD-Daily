c39435832e77106cabb5e14537b30410eeb13c5b
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2023-11-24 19:27:16 +0530

    November-2023

--- 004 GFG-POTD-Daily-November-2023/24 Pascal-Triangle.py
+++ 004 GFG-POTD-Daily-November-2023/24 Pascal-Triangle.py
@@ -2,20 +2,20 @@
 
 class Solution:
 
-    def nthRowOfPascalTriangle(self,n):
+    def nthRowOfPascalTriangle(self, n):
         # code here
-        mod = ((10**9) + 7)
-        results = [[] for i in range (n+1)]
-        #results[1] = [1]
-        if n == 1 : return [1]
-        results[2] = [1,1]
-        for i in range(3, n+1):
+        mod = ((10 ** 9) + 7)
+        results = [[] for i in range(n + 1)]
+        # results[1] = [1]
+        if n == 1: return [1]
+        results[2] = [1, 1]
+        for i in range(3, n + 1):
             results[i].append(1)
-            prev = results[i-1]
+            prev = results[i - 1]
             leftOne = 0
             rightOne = 1
-            while(rightOne < (i - 1)):
-                results [i] .append((prev[leftOne] + prev[rightOne]) % mod)
+            while rightOne < (i - 1):
+                results[i].append((prev[leftOne] + prev[rightOne]) % mod)
                 leftOne += 1
                 rightOne += 1
             results[i].append(1)
