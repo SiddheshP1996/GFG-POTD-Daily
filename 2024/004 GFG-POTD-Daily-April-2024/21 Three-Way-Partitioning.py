af481b51a8dbd46540b618637b23ec178d38c9f5
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-04-21 22:35:54 +0530

    Year-2024

---
+++ 2024/004 GFG-POTD-Daily-April-2024/21 Three-Way-Partitioning.py
@@ -0,0 +1,11 @@
+# User function template for Python
+
+class Solution:
+    """
+    Function to partition the array around the range such 
+    that array is divided into three parts.
+    """
+    def threeWayPartition(self, array, a, b):
+	    # Code here
+	    array.sort()
+	    return 1
