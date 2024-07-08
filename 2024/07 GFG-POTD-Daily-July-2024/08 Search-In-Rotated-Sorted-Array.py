a527bb844930fdd379f4147ec36a8c47e6a4e0c4
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-07-08 11:11:53 +0530

    Year-2024

---
+++ 2024/07 GFG-POTD-Daily-July-2024/08 Search-In-Rotated-Sorted-Array.py
@@ -0,0 +1,8 @@
+# User function Template for python3
+
+class Solution:
+    def search(self, arr, key):
+        # Complete this function
+        if key in arr:
+            return arr.index(key)
+        return -1
