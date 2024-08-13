4c3d14a21a39187cc11ba62ef9263437a5ed6d8d
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-08-13 19:31:45 +0530

    Year-2024

---
+++ 2024/08 GFG-POTD-Daily-August-2024/13 Square-Root-Of-A-Number.py
@@ -0,0 +1,9 @@
+# User function Template for python3
+
+from math import sqrt, floor
+
+# Complete this function
+class Solution:
+    def floorSqrt(self, n): 
+        # Your code here
+        return floor(sqrt(n))
