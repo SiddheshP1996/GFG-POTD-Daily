6e5d076b556d026645b6594a187675ebbd70df6c
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-09-19 23:27:49 +0530

    Year-2024

---
+++ 2024/09 GFG-POTD-Daily-September-2024/19 Reverse-Words.py
@@ -0,0 +1,8 @@
+# User function Template for python3
+
+class Solution:
+    
+    # Function to reverse words in a given string.
+    def reverseWords(self, str):
+        # Code here
+        return ".".join(str.split(".")[::-1])
